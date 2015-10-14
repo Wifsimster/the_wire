ec6d28e80c1e131cde181584890171a63496ce00
Damien BATTISTELLA <dbattistella@arca-computing.fr>
2015-10-14 20:52:43 +0200

    Improve lighting device description data.

--- public/js/models/lighting/door.js
+++ public/js/models/lighting/door.js
@@ -3,6 +3,39 @@ function Door(_object) {
     Device.call(this, _object);
 
     this.data === "Closed" ? this.open = false : this.open = true;;
+
+    this.addjMulti = _object.AddjMulti;
+    this.addjMulti2 = _object.AddjMulti2;
+    this.addjValue = _object.AddjValue;
+    this.addjValue2 = _object.AddjValue2;
+    this.batteryLevel = _object.BatteryLevel;
+    this.customImage = _object.CustomImage;
+    this.hardwareId = _object.HardwareID;
+    this.hardwareName = _object.HardwareName;
+    this.hardwareType = _object.HardwareType;
+    this.hardwareTypeVal = _object.HardwareTypeVal;
+    this.haveDimmer = _object.HaveDimmer;
+    this.haveGroupCmd = _object.HaveGroupCmd;
+    this.haveTimeout = _object.HaveTimeout;
+    this.id = _object.ID;
+    this.image = _object.Image;
+    this.internalState = _object.InternalState;
+    this.isSubDevice = _object.IsSubDevice;
+    this.level = _object.Level;
+    this.levelInt = _object.LevelInt;
+    this.maxDimLevel = _object.MaxDimLevel;
+    this.planId = _object.PlanID;
+    this.planIds = _object.PlanIDs;
+    this.showNotifications = _object.ShowNotifications;
+    this.signalLevel = _object.SignalLevel;
+    this.strParam1 = _object.StrParam1;
+    this.strParam2 = _object.StrParam2;
+    this.subType = _object.SubType;
+    this.switchType = _object.SwitchType;
+    this.switchTypeVal = _object.SwitchTypeVal;
+    this.timers = _object.Timers;
+    this.unit = _object.Unit;
+    this.used = _object.Used;
 }
 
 Door.prototype.isOpen = function () {
